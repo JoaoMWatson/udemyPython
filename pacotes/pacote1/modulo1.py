77ec5c48d75a9f50f83f131919902822a9fae630
Joao Pedro <joaopedrowatson@hotmail.com>
2019-07-25 11:42:50 -0300

    comitando de segurança

---
+++ pacotes/pacote1/modulo1.py
@@ -0,0 +1,3 @@
+def soma(a, b):
+    print('Importando módulo pacote1.modulo1 do pacote pacote1!')
+    return a + b
---
+++ pacotes/pacote_v1.py
@@ -0,0 +1,3 @@
+from pacote1 import modulo1
+
+print(f'A soma de a + b é igual a {modulo1.soma(2, 5)}')
